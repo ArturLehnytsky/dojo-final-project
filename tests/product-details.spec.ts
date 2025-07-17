655fc676ecb78ccd6b09b10fc69faa4be2bd7759
Lekhnitskyi <arturlehnytsky@gmail.com>
2025-07-17 23:07:41 +0300

    Add customizable worker count and tag test with metadata
    Updated the GitHub Action to accept a worker count as input, making the workflow more flexible. Adjusted the Playwright configuration to use the provided worker count. Added a tag for the test case to improve categorization and readability in reports.

--- tests/product-details.spec.ts
+++ tests/product-details.spec.ts
@@ -4,26 +4,26 @@ import { expect } from '@playwright/test';
 
 test.describe('Logged in user can change color and size on product details page', () => {
   test.use({ userToLogin: USERS.standard_user });
-  test('TC-04 User should be able to add all sizes to cart', async ({
-    homePage,
-    productPage,
-    cartPage,
-  }) => {
-    const productName = 'Hummingbird printed t-shirt';
-    let sizeArrayAdded = [];
+  test(
+    'TC-04 User should be able to add all sizes to cart',
+    { tag: ['@product-page'] },
+    async ({ homePage, productPage, cartPage }) => {
+      const productName = 'Hummingbird printed t-shirt';
+      let sizeArrayAdded = [];
 
-    await test.step('User uses search to find product on the home page and click on it', async () => {
-      await homePage.findProductAndClickOnIt(productName);
-    });
+      await test.step('User uses search to find product on the home page and click on it', async () => {
+        await homePage.findProductAndClickOnIt(productName);
+      });
 
-    await test.step('User on product page and adds all sizes to cart', async () => {
-      sizeArrayAdded = (await productPage.addAllSizesToCart()).slice();
-    });
+      await test.step('User on product page and adds all sizes to cart', async () => {
+        sizeArrayAdded = (await productPage.addAllSizesToCart()).slice();
+      });
 
-    await test.step('User on cart page and checks that all sizes are added', async () => {
-      await productPage.header.goToCartPage();
-      const actualSizes = await cartPage.getSizeOffAllProducts();
-      expect(actualSizes).toEqual(sizeArrayAdded);
-    });
-  });
+      await test.step('User on cart page and checks that all sizes are added', async () => {
+        await productPage.header.goToCartPage();
+        const actualSizes = await cartPage.getSizeOffAllProducts();
+        expect(actualSizes).toEqual(sizeArrayAdded);
+      });
+    }
+  );
 });
